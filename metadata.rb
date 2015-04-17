52d44cbca73cd13b9576535578cf576f1e4d3d46
Sinister Light <me@sinisterlight.com>
2015-04-17 11:49:29 +0530

    [Shobhit] scaffold

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'monitoring-cookbook'
+maintainer       'Shobhit Srivastava'
+maintainer_email 'me@sinisterlight.com'
+license          'All rights reserved'
+description      'Installs/Configures monitoring-cookbook'
+long_description 'Installs/Configures monitoring-cookbook'
+version          '0.0.0'
+
